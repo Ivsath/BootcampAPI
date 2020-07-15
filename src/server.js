7180f335df20ed3163ee87b333dd02771b5d1e53
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-15 17:42:24 -0500

    Connect the database with mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+
 import bootcamps from "./routes/bootcamps";
+
 import config from "./config";
+import connect from "./utils/db";
 
 export const app = express();
 
@@ -11,10 +14,15 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/v1/bootcamps", bootcamps);
 
-export const start = () => {
-  app.listen(config.port, () => {
-    console.log(
-      `Server running in ${config.env} mode on http://localhost:${config.port}`
-    );
-  });
+export const start = async () => {
+  try {
+    await connect();
+    app.listen(config.port, () => {
+      console.log(
+        `Server running in ${config.env} mode on http://localhost:${config.port}`
+      );
+    });
+  } catch (e) {
+    console.error(e);
+  }
 };
