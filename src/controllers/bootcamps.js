e6101694f4a870f534b895ebf2ccbda2ad357b7e
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-23 11:58:39 -0500

    Implement post endpoint

--- src/controllers/bootcamps.js
+++ src/controllers/bootcamps.js
@@ -1,3 +1,5 @@
+import Bootcamp from "../models/Bootcamp";
+
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
@@ -8,8 +10,14 @@ export const getBootcamps = (req, res) => {
 // @desc      Create new bootcamp
 // @route     POST /api/v1/bootcamps
 // @access    Public
-export const createBootcamp = (req, res) => {
-  res.status(200).json({ success: true });
+export const createBootcamp = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc      Get a single bootcamp
