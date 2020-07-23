bb11ce85834eb6444cdaf70c8117a027385cf321
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-23 18:42:31 -0500

    Implement bootcamp get request handlers

--- src/controllers/bootcamps.js
+++ src/controllers/bootcamps.js
@@ -3,8 +3,14 @@ import Bootcamp from "../models/Bootcamp";
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
 // @access    Public
-export const getBootcamps = (req, res) => {
-  res.status(200).json({ success: true });
+export const getBootcamps = async (req, res) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc      Create new bootcamp
@@ -23,8 +29,18 @@ export const createBootcamp = async (req, res) => {
 // @desc      Get a single bootcamp
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
-export const getBootcamp = (req, res) => {
-  res.status(200).json({ success: true });
+export const getBootcamp = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc      Update bootcamp
