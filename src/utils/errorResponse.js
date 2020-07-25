22db857b8d82e7c5b51ca037cbab1852ed26f479
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-25 15:13:11 -0500

    Add name field to the ErrorResponse class

--- src/utils/errorResponse.js
+++ src/utils/errorResponse.js
@@ -2,6 +2,8 @@ class ErrorResponse extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
+    // Ensure the name of this error is the same as the class name
+    this.name = this.constructor.name;
   }
 }
 
