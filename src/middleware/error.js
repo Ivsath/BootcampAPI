aa52aa14aec919536e6def3bfffafd0f340feafa
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-25 12:13:05 -0500

    Handle cast errors in the middleware

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,9 +1,18 @@
+import ErrorResponse from "../utils/errorResponse";
+
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
   console.log(err.stack);
 
+  if (err.name === "CastError") {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
   res
-    .status(err.statusCode || 500)
-    .json({ success: false, error: err.message || "Server Error" });
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || "Server Error" });
 };
 
 export default errorHandler;
