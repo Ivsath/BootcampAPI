8f8f485dd5ae1d764c853301349ce6a09251f814
Irvin Reyes <reyes.yerp@gmail.com>
2020-07-25 15:30:25 -0500

    Add mongoose validation to the error custom middleware

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -2,12 +2,25 @@ import ErrorResponse from "../utils/errorResponse";
 
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
+  error.message = err.message;
 
   console.log(err.stack);
 
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res
